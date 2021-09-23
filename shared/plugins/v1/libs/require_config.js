67554c3023f55c5ccb934ee979ff2873992b3bc1
janhenrikejme <jan.ejme@gmail.com>
2021-09-23 17:34:40 +0200

    sd

--- shared/plugins/v1/libs/require_config.js
+++ shared/plugins/v1/libs/require_config.js
@@ -6,7 +6,7 @@ requirejs.onError = function (err) {
   
   require.config({
    // urlArgs: "_=" + new Date().getTime(),
-    baseUrl: './libs',
+    baseUrl: './',
     paths: {
       'jquery': 'https://eesy.github.io/shared/plugins/v1/libs/jquery-3.3.1.min',
       'iframe_communicator_client': 'https://eesy.github.io/shared/plugins/v1/libs/iframe_communicator_client',
