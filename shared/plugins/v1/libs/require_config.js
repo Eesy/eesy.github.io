6da4d61e4b0aed1b57a7648cf3011f362e1e1fec
janhenrikejme <jan.ejme@gmail.com>
2021-09-23 22:17:58 +0200

    s

--- shared/plugins/v1/libs/require_config.js
+++ shared/plugins/v1/libs/require_config.js
@@ -10,8 +10,7 @@ requirejs.onError = function (err) {
     paths: {
       'jquery': 'https://eesy.github.io/shared/plugins/v1/libs/jquery-3.3.1.min',
       'iframe_communicator_client': 'https://eesy.github.io/shared/plugins/v1/libs/iframe_communicator_client',
-      'canvas_api': 'https://eesy.github.io/shared/plugins/v1/libs/canvas_api',
-      'resources_': './resources',
+      'canvas_api': 'https://eesy.github.io/shared/plugins/v1/libs/canvas_api'
     }
   });
   
